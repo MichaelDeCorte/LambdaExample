21b360f108d41e583f1c2044ac449adf64c9e4ef
Michael DeCorte <mdecorte.decorte+github@gmail.com>
2018-04-09 22:28:52 -0400

    putParty lambda/dynamo service with unit and integration tests

---
+++ HelloWorld.terraform/partyTable.tf
@@ -0,0 +1,22 @@
+# partyTable.tf
+    
+resource "aws_dynamodb_table" "partyTable" {
+  name           = "party"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "partyID"
+  range_key      = "lastName"
+
+  attribute {
+    name = "partyID"
+    type = "N"
+  }
+
+  attribute {
+    name = "lastName"
+    type = "S"
+  }
+
+  tags    = "${module.variables.tags}"
+}
+
