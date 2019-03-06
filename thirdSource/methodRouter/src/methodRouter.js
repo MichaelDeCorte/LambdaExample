e7517226a79255014a47bdc2e491ea95e2bf1f0f
Michael DeCorte <mdecorte.decorte+github@gmail.com>
2019-03-06 17:02:03 -0500

    fixed CORS issue

--- thirdSource/methodRouter/src/methodRouter.js
+++ thirdSource/methodRouter/src/methodRouter.js
@@ -197,8 +197,8 @@ function methodRouter(event, context, callback, map) {
                     }
                 );
                 
-                // result.headers['Access-Control-Allow-Origin'] = '*';
-                // result.headers['Access-Control-Allow-Credentials'] = '*';
+                result.headers['Access-Control-Allow-Origin'] = '*'; // call from any browser
+                result.headers['Access-Control-Allow-Credentials'] = '*';
                 logger.trace('return');
                 callback(error, result);
             })
@@ -215,8 +215,8 @@ function methodRouter(event, context, callback, map) {
                 
                 logger.error('error: ' + JSON.stringify(error, null, 4));
                 logger.error('result: ' + JSON.stringify(result, null, 4));
-                // result.headers['Access-Control-Allow-Origin'] = '*';
-                // result.headers['Access-Control-Allow-Credentials'] = '*';
+                result.headers['Access-Control-Allow-Origin'] = '*'; // call from any browser
+                result.headers['Access-Control-Allow-Credentials'] = '*';
                 callback(error, result);
             });
 }
