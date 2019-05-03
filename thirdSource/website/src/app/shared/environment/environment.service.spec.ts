590b07fe919334ab4fd32647603e8c28fec21d43
Michael DeCorte <mdecorte.decorte+github@gmail.com>
2019-05-03 12:58:58 -0400

    initial angular / jasmine test

--- thirdSource/website/src/app/shared/environment/environment.service.spec.ts
+++ thirdSource/website/src/app/shared/environment/environment.service.spec.ts
@@ -1,12 +1,56 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+
+import {
+    HttpClientTestingModule,
+    HttpTestingController
+       } from '@angular/common/http/testing';
 
 import { EnvironmentService } from './environment.service';
 
 describe('EnvironmentService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [EnvironmentService]
+        });
+    });
 
-  it('should be created', () => {
-    const service: EnvironmentService = TestBed.get(EnvironmentService);
-    expect(service).toBeTruthy();
-  });
+    it('should be initialized',
+       inject([EnvironmentService], 
+              (environmentService: EnvironmentService) => {
+                  expect(environmentService).toBeTruthy();
+              }
+             )
+      );
+
+    it('should get environment',
+       fakeAsync(
+           inject([EnvironmentService, HttpTestingController], 
+                  (environmentService: EnvironmentService, http: HttpTestingController) => {
+                      const url = '/assets/environment.dev.json';
+                      const responseObject = {
+                          "apiEndPoints": {"party":{"endpoint":"/party"}},
+                          "apiInvokeUrl": "https://dh9qp202xf.execute-api.us-east-1.amazonaws.com/dev-stage",
+                          "cognitoUserPoolId": "us-east-1_JQpUwdflR",
+                          "cognitoClientId": "7it5nphstn03r67faklvdjsd33",
+                          "cognitoUrl": "https://thirdsource.auth.us-east-1.amazoncognito.com",
+                          "loginUrl": "https://thirdsource.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=7it5nphstn03r67faklvdjsd33&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fsecurity%2Fauthenticate",
+                          "region": "us-east-1"
+                      };
+
+                      environmentService.initialize();
+
+                      const requestWrapper = http.expectOne({url: url});
+                      requestWrapper.flush(responseObject);
+                      tick();
+
+                      let config = environmentService.getConfig();
+
+                      expect(config).toBeTruthy();
+                      expect(config).toEqual(responseObject);
+                  }
+                 )
+       )
+       );
 });
